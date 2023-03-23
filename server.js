7c2aa7effebb10191f4d8d485807ee68dd4968ab
Harrison Tran-Chung <tranchung@live.com>
2023-03-22 22:45:34 -0500

    added app listening for easier use

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
 });
 
 
