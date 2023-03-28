812dac4740b5f96f9773eabc9e53dfad9cdb5d84
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-03-28 18:47:56 -0500

    Changes to handlebars pages

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,37 @@
 const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
-const userRoutes = require('../routes/api/userRoutes');
-const signUp = require('./signUp');
-const saveCard = require('../routes/api/userRoutes');
-const apiRoutes = require('./api');
-const data = require('../seeds/targetLocations.json');
-// Allows handlebars to render templates (Keep here for now)
-router.get("/", (req, res) => {
-  res.render("layouts/main");
-});
+const userRoutes = require("../routes/api/userRoutes");
+const signUp = require("./signUp");
+const apiRoutes = require("./api");
+const data = require("../seeds/targetLocations.json");
 
-router.use("/saveCard", saveCard)
+// User Routes to pages
 router.use("/", homeRoutes);
-router.use('/', userRoutes);
+router.use("/", userRoutes);
 router.use("/", signUp);
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
-router.get('/api/:value',  (req, res) => {
+router.get("/api/:value", (req, res) => {
   const search = req.params.value;
 
   const targetObject = data.find(
-    obj => obj.tNumber.toLowerCase() === search.toLowerCase() || obj.address.toLowerCase() === search.toLowerCase()
+    (obj) =>
+      obj.tNumber.toLowerCase() === search.toLowerCase() ||
+      obj.address.toLowerCase() === search.toLowerCase()
   );
 
   if (targetObject) {
-    res.json(targetObject);
+    const filteredObject = {
+      tNumber: targetObject.tNumber,
+      address: targetObject.address,
+      city: targetObject.city,
+      state: targetObject.state,
+      postal: targetObject.postal,
+    };
+    res.json(filteredObject);
   } else {
     res.status(404).send("T-Number OR Address Not Found!");
   }
 });
 
 module.exports = router;
-
-
-
