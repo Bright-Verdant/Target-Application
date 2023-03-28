b3f08e6d6d749ce60588aa7871779bc6aaa7d9e8
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-03-27 20:37:21 -0500

    Cleaned up routing to pages

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,13 @@ const userRoutes = require('../routes/api/userRoutes');
 const signUp = require('./signUp');
 const apiRoutes = require('./api');
 const data = require('../seeds/targetLocations.json');
-// Allows handlebars to render templates (Keep here for now)
-router.get("/", (req, res) => {
-  res.render("layouts/main");
-});
 
+// User Routes to pages
 router.use("/", homeRoutes);
 router.use('/', userRoutes);
 router.use("/", signUp);
 router.use('/api', apiRoutes);
+
 
 router.get('/api/:value',  (req, res) => {
   const search = req.params.value;
