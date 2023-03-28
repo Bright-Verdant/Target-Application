5faaa63f2b7c79451c63656a8b039b1f1cb4e8f2
Harrison Tran-Chung <Tranchung@live.com>
2023-03-27 22:26:59 -0500

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    // this hook hashes the password before it's seeded in the db and returns it as the object newUserData
+    hooks: {
+      async beforeCreate(newUserData) {
+
+        newUserData.username = newUserData.email;
+// the above line makes username = to email so when inserting data we only need to utilize email, this can be changed if we ever want to utilize both separately by simply deleting the above line "newUserData.username = newUserData.email;"
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
